2ebf26e809c5bc61eb09ca3dcc29538e35534565
Justin Crain <131824461+Jcrain5418@users.noreply.github.com>
2023-11-20 16:40:31 -0600

    b

--- tic_tac.js
+++ tic_tac.js
@@ -1,16 +1,11 @@
-
-
 const cell = document.querySelectorAll('.cell')
-const startBtn = document.querySelector('.start_btn')
-
-startBtn.addEventListener('click', () =>{
-  alert("hey")
-})
-
-function handleClick(e){
-  e.target.textContent = "x"
-}
-
 cell.forEach((cell => {
-  cell.addEventListener('click', handleClick, {once: true})
+    cell.addEventListener('click', handleClick, {once: true})
 }))//will handle only letting cell be clicked once
+
+
+
+
+function handleClick(){
+    
+}
